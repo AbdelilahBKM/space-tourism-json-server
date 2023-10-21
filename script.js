1f40df41071b12ac56f163bfc754a6acd4070444
HOPE <boukhatemabdelilah420@gmail.com>
2023-10-21 20:55:48 +0100

    fixed path

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
+console.log('http://localhost:8080/')
 server.use(middlewares);
 server.use(router);
 
